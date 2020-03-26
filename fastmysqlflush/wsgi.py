8b41701d75f64397e0f25e60a9e5810c1286a497
c-bata <contact@c-bata.link>
2020-03-26 23:11:37 +0900

    init

---
+++ fastmysqlflush/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fastmysqlflush project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fastmysqlflush.settings')
+
+application = get_wsgi_application()
